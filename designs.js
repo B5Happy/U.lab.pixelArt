7366cf7e11b51b15fa63d4a723f3b05335ec3210
Saly <saly@juke4me.com>
2018-05-18 05:16:35 +0200

    Make a grid

--- designs.js
+++ designs.js
@@ -7,8 +7,12 @@ var gHeight = document.getElementById("inputHeight").value;
 
 // When size is submitted by the user, call makeGrid()
 document.getElementById("sizePicker").addEventListener("submit", makeGrid);
+document.getElementById("pixelCanvas").addEventListener("click", draw);
+//document.getElementsByTagName("td").addEventListener("click", draw);
 
 function makeGrid() {
+
+  event.preventDefault();
 
   var gWidth = document.getElementById("inputWidth").value;
   var gHeight = document.getElementById("inputHeight").value;
@@ -16,13 +20,36 @@ function makeGrid() {
   var table = document.getElementById("pixelCanvas");
   	
   	 for (var i = 0; i < gHeight; i++) {
-     var tr = document.createElement('tr');
+     var tr = document.createElement("tr"); 
      for (var j = 0; j < gWidth; j++) {
-         var td = document.createElement('td');
+         var td = document.createElement("td");
      tr.appendChild(td);
      }
     table.appendChild(tr);
     }
+   
 
   
 }
+
+function draw() {
+
+  const PicColors = document.getElementById("colorPicker").value;
+
+  //const InitColor = document.getElementsByClassName("test");
+
+//InitColor.color = colors;
+
+document.getElementsByTagName("td").style.background = black ;
+
+
+  alert(PicColors);
+
+}
+
+//function chBackcolor(color) {
+//
+ // const colors = document.getElementById("colorPicker").value;
+  
+//  document.getElementsByTagName(td).color = colors;
+//}
